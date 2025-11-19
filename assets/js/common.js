7851b6659d1be78f0c6ae14b98df9ff9195173c3
Bikyoung Shon <103485405+Bikyoung@users.noreply.github.com>
2025-11-19 13:29:46 +0900

    create vision section in index.html

---
+++ assets/js/common.js
@@ -0,0 +1,9 @@
+gsap.registerPlugin("ScrollTrigger, ScrollSmoother");
+
+ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 2,
+    effects: true
+});
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,43 @@
-gsap.registerPlugin(ScrollTrigger);
-
-let visionAnimation = gsap.timeline({
+// .vision__title이 스크롤에 맞춰 아래에서 올라오며 fade-in
+const fadeInVisionTitle = gsap.from(".vision__title", {
+    y: "100%",
+    autoAlpha: 0,
+    ease: "none",
     scrollTrigger: {
-        trigger: ".vision__container--top",
-        start: "top top",
-        end: "+=3000",
-        scrub: true,
-        pin: true,
-        anticipatePin: 1,
-        markers: true
+        trigger: ".vision__top",
+        start: "top 30%",
+        end: "+=30%",
+        scrub: 2.5
     }
 });
 
-visionAnimation.to(".vision__title", {
-    color: "#4E4E4E",
-    scale: 0 
+// .vision__top을 스크롤 중 잠시 고정
+ScrollTrigger.create({
+    trigger: ".vision__top",
+    start: "10% top",
+    end: "+=1000",
+    pin: true,
+    anticipatePin: 1,
+});
+
+// .vision__title이 스크롤에 맞춰 fade-out 되는 timeline
+const fadeOutVisionTimeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".vision__middle",
+        start: "top 50%",
+        end: "+=500",
+        scrub: 3
+    }
+});
+
+fadeOutVisionTimeline.to(".vision__title", {
+    scale: 0
+}, 0);
+fadeOutVisionTimeline.to(".vision__title", {
+    autoAlpha: 0
 }, 0);
 
-visionAnimation.to(".vision__container--bottom", {
-    top: "20%"
-}, 0.2);
+
+
+
 
