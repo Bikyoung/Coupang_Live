c47658255cac46bb8d48c07cd3d5a0624edf3e6e
Bikyoung Shon <103485405+Bikyoung@users.noreply.github.com>
2026-01-01 20:28:34 +0900

    create header.html & m-nav.html

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,21 +1,77 @@
 import { loadComponent } from "./componentLoader.js";
 
-if(typeof gsap !== "undefined" && typeof ScrollTrigger !== "undefined") {
-    gsap.registerPlugin("ScrollTrigger, ScrollSmoother");
+async function init() {
+    try {
+        // header와 footer 로드 및 UI 설정  
+        let pageName = document.querySelector("body").dataset.page;
+
+        await loadComponent("header");
+        await loadComponent("m-nav");
+        await loadComponent("footer");
     
-    ScrollSmoother.create({
-        wrapper: "#smooth-wrapper",
-        content: "#smooth-content",
-        smooth: 2,
-        effects: true,
-        // 브라우저의 불안정한 스크롤 떨림 현상을 줄여줌
-        normalizeScroll: true
+        const header = document.querySelector("header");
+        const menuList = document.querySelectorAll("header nav a");
+
+        applyHeaderStyle(pageName, header, menuList);
+        setAriaCurrent(pageName, menuList);
+        toggleMobileMenu();
+
+        // GSAP 설정
+        if(typeof gsap !== "undefined" && typeof ScrollTrigger !== "undefined") {
+            gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+            
+            ScrollSmoother.create({
+                wrapper: "#smooth-wrapper",
+                content: "#smooth-content",
+                smooth: 2,
+                effects: true,
+                // 브라우저의 불안정한 스크롤 떨림 현상을 줄여줌
+                normalizeScroll: true
+            });
+        }
+
+        
+    } catch(error) {
+        console.error("header와 footer 로드 실패", error);
+    }
+}
+
+// 페이지별 header의 스타일을 다르게 적용
+function applyHeaderStyle(pageName, header, menuList) {
+    if((pageName === "faq") || (pageName === "contact")) {
+        header.style.borderColor = "#C0C0C0";
+        menuList.forEach((menu) => {
+            menu.style.color = "#000715"
+        });
+    }
+}
+
+// header nav a에 웹 접근성 준수
+function setAriaCurrent(pageName, menuList) {
+    menuList.forEach((menu) => {
+        if(menu.dataset.menu === pageName) {
+            menu.setAttribute("aria-current", "page");
+            menu.style.color = "#F7552F";
+        }
+    })
+}
+
+// .m-nav에 show 클래스 토글
+function toggleMobileMenu() {
+    const menuBtns = document.querySelectorAll(".menu-toggle");
+    const mobileMenu = document.querySelector(".m-nav");
+
+    menuBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            mobileMenu.classList.toggle("show");
+        })
     });
 }
 
-["header", "footer"].forEach(
-    (component) => {loadComponent(component);}
-)
+init();
+
+
+    
 
 
 
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,42 +3,4 @@ $(function() {
     $(".agreement__check i").on("click", function() {
         $(this).closest(".agreement__check").toggleClass("check");
     });
-
-    // $("form").on("submit", function() {
-    //     preventDefault();
-        
-    //     if(!$("#category").validity.valid) {
-
-    //     }
-    // });
 });
-
-// 유효성 메시지 팝업 등장
-// Form.addEventListener("submit", (event) => {
-//     Body.style.overflow = "hidden";
-//     /* preventDefault() 
-//        : 해당 이벤트의 기본 동작을 막는 함수
-//          submit의 기본 동작은 1.유효성 검사 2.유효성 에러 메시지 3. 제출(페이지 새로고침)인데,
-//          form 태그에 명시된 novalidate로 인해 1,2이 생략되어 바로 form 제출을 하게 됨
-//          하지만 JS에서 유효성 검사를 하려면 바로 제출되는 것을 방지해야 하므로 preventDefault()를 호출함 */
-//     event.preventDefault();
-
-//     if (!InputUserName.validity.valid) {
-//         showPopUp("이름은 필수 항목입니다.");
-//     } else if (!InputPhoneNumber.validity.valid) {
-//         // 연락처를 입력하지 않은 경우
-//         if (InputPhoneNumber.validity.valueMissing) {
-//             showPopUp("연락처는 필수 항목입니다.");
-//         }
-//         // 입력 형식이 맞지 않은 경우
-//         else if (InputPhoneNumber.validity.patternMismatch) {
-//             showPopUp("연락처는 '-' 없이 숫자 11자리만 입력해야 합니다.\n(예: 01012345678)");
-//         }
-//     } else if (!InputContent.validity.valid) {
-//         showPopUp("문의 내용은 필수 항목입니다.");
-//     } else if (!InputPrivacyCheck.validity.valid) {
-//         showPopUp("본 동의는 서비스 이용을 위해 필수입니다.");
-//     } else {
-//         showPopUp("문의가 접수되었습니다.\n담당자가 확인 후 순차적으로 답변드릴 예정입니다.");
-//     }
-// });
