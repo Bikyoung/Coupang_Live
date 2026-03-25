e216f77f333b883aba260ebba755a9d840779289
Bikyoung Shon <103485405+Bikyoung@users.noreply.github.com>
2026-03-25 10:55:00 +0900

    fix(contact): correct form validation logic

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,87 +1,71 @@
 import { commonInit } from "./common.js";
 
 async function contactInit() {
-    const form = document.querySelector("form");
-    const inputGroup = document.querySelectorAll(".input-group"); 
-    const checkBox = document.querySelector("#agreement__checkbox");
-    const checkBoxIcon = document.querySelector(".fa-square-check");
-
+    const contactForm = document.querySelector(".contact__form");
+    const inputList = document.querySelectorAll(".input");
     let gsapMatchMedia = gsap.matchMedia();
+    let isSubmitted = false;    // 페이지 로드 이후 폼이 제출된 적 있었는지 여부를 저장
 
-    await commonInit(); 
+    await commonInit();
 
-    // .agreement__check i를 클릭 시, 체크 상태 toggle
-    checkBox.addEventListener("change", function() {
-        checkBoxIcon.classList.toggle("check");
-    });
-
-    // 화면 너비가 1441px 이상일 때, .container__left에 pin 기능 적용
+    // 화면 너비가 1441px 이상일 때, .contact__title-wrapper에 pin 기능 적용
     gsapMatchMedia.add("(min-width: 1441px)", () => {
         ScrollTrigger.create({
-            trigger: ".container__left",
-            start: "top top",
+            trigger: ".contact__title-wrapper",
+            start: "top 20%",
             end: "bottom 35%",
-            pin: true,
+            pin: true
         });
     });
 
-    /* 폼 입력 요소에 blur, change, input 이벤트 발생 시, 
-        해당 요소에 실시간 유효성 검사를 수행 
-        (폼 재출 시 수행하는 유효성 검사와는 별도) */
-    inputGroup.forEach((input) => {
-        const field = input.querySelector("select, input, textarea");
+    contactForm.addEventListener("submit", (e) => {
+        let firstErrorInput = null;    // 첫번째 에러 입력 요소를 저장
 
-        ["blur", "change", "input"].forEach((eventType) => {
-            field.addEventListener(eventType, () => {
-                checkValidation(input);
-            });
-        });
-    });
-
-    /* 폼에 submit 이벤트 발생 시, 유효성을 검사하여 유효하지 않은 첫번째 요소를 포커스 함
-        모든 폼 입력 요소가 유효할 시, 폼 제출 */
-    form.addEventListener("submit", (e) => {
-        let firstInvalidField = null;
-
+        isSubmitted = true;
+        /* 브라우저가 서버에 데이터를 전송하고 새로고침 하는 것을 방지
+           JS에서 커스텀 에러 메시지를 보여주거나 브라우저의 부족한 유효성 검사를 JS가 보완할 수 있도록 통제권을 확보 */
         e.preventDefault();
+ 
+        // 유효성 검사 결과에 따른 첫번째 에러 입력 요소 포커싱 또는 폼 제출 처리
+        inputList.forEach(input => {
+            validate(input);
 
-        inputGroup.forEach((input) => {
-            const isValid = checkValidation(input);
-
-            if(!isValid && !firstInvalidField) {
-                firstInvalidField = input.querySelector("select, input, textarea");
+            if(!input.validity.valid && !firstErrorInput) {
+                firstErrorInput = input;
             }
         });
-        
-        if(firstInvalidField) {
-            firstInvalidField.focus();
-        } else {
-            form.submit();
-        }
+
+        firstErrorInput ? firstErrorInput.focus() : contactForm.submit();
+    });
+
+    /* 최초 폼 제출 이후부터 모든 입력 요소에 실시간으로 유효성 검사에 따른 UI 업데이트 수행
+       이벤트 위임을 활용 */
+    ["input", "change", "focusout"].forEach(eventType => {
+        contactForm.addEventListener(eventType, (e) => {
+            const targetInput = e.target.closest(".input");
+
+            if(isSubmitted && targetInput) {
+                validate(targetInput);
+            }
+        });
     });
 }
 
-// 폼 입력 요소의 유효성 검사 함수
-function checkValidation(input) {
-    const field = input.querySelector("select, input, textarea");
-    const errorMessage = input.querySelector(".error-message");
-    
-    if(field.validity.valueMissing) {
-        const errRequired = field.dataset.errRequired || "필수 입력 항목입니다.";
+// 브라우저의 유효성 검사에 따른 UI 업데이트 함수
+function validate(input) {
+    const errorMessageTag = input.parentElement.querySelector(".error-message");
+    let errorMessage = "";
 
-        input.classList.add("is-invalid");
-        errorMessage.innerText = errRequired;
-
-        return false;
-
-    } else {
-        input.classList.remove("is-invalid");
-        return true;
+    if(input.validity.valueMissing || input.value.trim() ==="") {
+        const action = input.dataset.action;
+        errorMessage = `필수 ${action} 항목입니다`;
+    } else if(input.validity.patternMismatch) {
+        const format = input.dataset.format;
+        errorMessage = `${format} 형식이 올바르지 않습니다`;
     }
+
+    errorMessageTag.textContent = errorMessage;
+    input.style.borderColor = errorMessage ? "#FF0000" : "#C0C0C0";
 }
 
 contactInit();
-
-
-
-
